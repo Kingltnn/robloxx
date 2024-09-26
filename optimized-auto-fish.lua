a59d0664f50616dad6ae85e16a51e4d13ee56790
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2024-09-26 18:18:54 +0700

    Update optimized-auto-fish.lua

--- optimized-auto-fish.lua
+++ optimized-auto-fish.lua
@@ -1,6 +1,6 @@
+repeat wait(5) until game:IsLoaded()
+
 print("Made By firedevil (Ryan | 404678244215029762 | https://discord.gg/ettP4TjbAb)")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/waitForGameLoad.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/antiStaff.lua"))()
 
 local Network = game:GetService("ReplicatedStorage"):WaitForChild("Network")
 
