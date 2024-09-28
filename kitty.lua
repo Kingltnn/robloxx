07b6f4b6deb412593d6a1697d8bed6084e24e0c4
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2024-09-28 14:27:03 +0700

    Create kitty.lua

---
+++ kitty.lua
@@ -0,0 +1,21 @@
+getgenv().KiTTYWARE_Elemental = {
+    Farm_Normal_Breakables = true,
+    Farm_Special_Breakables = true, 
+    Pets_Per_Breakable = 2,
+
+    Egg_Number = 241,
+    Auto_Hatch = true,
+    Auto_Mail_Huges = false,
+    Auto_Mail_Username = "",
+
+    Only_Catch_Huges = true, -- If false will look for Huges, then Quest-Pets, then Easiest/Highest % Chance
+    Auto_Catch_Pets = fale,
+    Auto_Cube_Merchant = true,
+
+    Discord_UserID = "",
+    Discord_WebhookUrl = "https://discord.com/api/webhooks/1289487530470674442/-4cIRau7IP0K4lU6Uygu8pgluiOChP89h0uvl2zoCXT6jndY-0tpWKN_OxnM5vX-yQ8o",
+    Fps_Boost = true,
+}
+
+script_key = "tssadevapGtmhaafIBLjornYmliCYDfh";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2d2d7d3e5cbec16ec501a759f3830ce.lua"))()
