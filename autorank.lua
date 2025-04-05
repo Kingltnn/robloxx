cf3cb23896c0d1a1f3b354153d0814574223086a
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-04-05 23:54:06 +0700

    Update autorank.lua

--- autorank.lua
+++ autorank.lua
@@ -1,13 +1,13 @@
 getgenv().Config = {
-    ["EquipEnchants"] = {"Strong Pets", "Strong Pets", "Strong Pets", "Strong Pets", "Strong Pets"},
+    ["EquipEnchants"] = {},
     ["KickWhenReachGoal"] = {
         ["Rank"] = 30,
         ["Rebirth"] = 9,
-        ["Area"] = 200
+        ["Area"] = 239
     },
     ["StopAt"] = {
         ["Rank"] = 30, 
-        ["Area"] = 200
+        ["Area"] = 239
     },
     
     ["AutoOpen"] = true,
@@ -15,6 +15,6 @@ getgenv().Config = {
 
     ["FPSBooster"] = true, -- REPORT NO SCRIPT BUGS.
     ["UltraFPS"] = true, -- I WILL BAN YOU IF YOU REPORT ANY SCRIPT BUGS.
-    ["Webhook"] = "https://discord.com/api/webhooks/1326777560947757076/aW0lAINHY0Exfuax-JmRle7vZgzWnHGxIXolwQJy4pe6EoSJwTy9OuICeCqPK0y3QteV", -- DO NOT REPORT ANY SCRIPT BUGS. WE KNOW ITS BUGGED.
+    ["Webhook"] = "", -- DO NOT REPORT ANY SCRIPT BUGS. WE KNOW ITS BUGGED.
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/177d65e916988f4b84dd21ed33a2aa91.lua"))()
